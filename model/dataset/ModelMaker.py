0072aeef36fd0ef115cd385bd1a3321dfa7a48b5
rOceanGG <roshangg@gmail.com>
2025-01-02 04:18:39 +0000

    Added getFaces function

--- model/dataset/ModelMaker.py
+++ model/dataset/ModelMaker.py
@@ -10,8 +10,8 @@ class ModelMaker:
     def __init__(self):
         self.faceCascade = cv2.CascadeClassifier('DriverIdentifier/model/opencv/haarcascade/haarcascade_frontalface_default.xml')
         self.eyeCascade  = cv2.CascadeClassifier('DriverIdentifier/model/opencv/haarcascade/haarcascade_eye.xml')
-        self.DATAPATH = "./model/dataset"
-        self.CROPPEDDATAPATH = "./model/dataset/cropped"
+        self.DATAPATH = "./model/dataset/"
+        self.CROPPEDDATAPATH = "./model/dataset/cropped/"
 
     def getFaces(self):
         #This function will be used on individual images to grab the faces of the drivers
@@ -25,3 +25,38 @@ class ModelMaker:
                 eyes = self.eyeCascade.detectMultiScale(regionOfInterestGray)
                 if len(eyes) >= 2:
                     return regionOfInterestColour
+        
+        ImageDirectories = []
+        for entry in os.scandir(self.DATAPATH):
+            if entry.is_dir():
+                ImageDirectories.append(entry.path)
+        
+        if os.path.exists(self.CROPPEDDATAPATH):
+            shutil.rmtree(self.CROPPEDDATAPATH)
+        
+        CroppedImageDirectories = []
+        DriverFileNamesDictionary = {}
+
+        for imageDirectory in ImageDirectories:
+            count = 1
+            driver = imageDirectory.split('/')[-1]
+            DriverFileNamesDictionary[driver] = []
+            croppedFolder = self.CROPPEDDATAPATH + driver
+            if not os.path.exists(croppedFolder):
+                os.makedirs(croppedFolder)
+            
+            CroppedImageDirectories.append(croppedFolder)
+
+            for entry in os.scandir(imageDirectory):
+                try:
+                    croppedImage = getCroppedImageWithTwoEyes(entry.path)
+                except:
+                    croppedImage = None
+                
+                if croppedImage:
+                    newFileName = driver + str(count) + ".png"
+                    newFilePath = croppedFolder + "/" + newFileName
+
+                    cv2.imwrite(newFilePath, croppedImage)
+                    DriverFileNamesDictionary[driver].append(newFileName)
+                    count += 1
