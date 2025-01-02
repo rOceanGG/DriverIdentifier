991fd2041c5fcf18557882bb10a558c5cc1536c7
rOceanGG <roshangg@gmail.com>
2025-01-02 03:54:15 +0000

    Added function for grabbing cropped images of drivers as long as they have two visible eyes

--- model/dataset/ModelMaker.py
+++ model/dataset/ModelMaker.py
@@ -10,3 +10,18 @@ class ModelMaker:
     def __init__(self):
         self.faceCascade = cv2.CascadeClassifier('DriverIdentifier/model/opencv/haarcascade/haarcascade_frontalface_default.xml')
         self.eyeCascade  = cv2.CascadeClassifier('DriverIdentifier/model/opencv/haarcascade/haarcascade_eye.xml')
+        self.DATAPATH = "./model/dataset"
+        self.CROPPEDDATAPATH = "./model/dataset/cropped"
+
+    def getFaces(self):
+        #This function will be used on individual images to grab the faces of the drivers
+        def getCroppedImageWithTwoEyes(imagePath):
+            img = cv2.imread(imagePath)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = self.faceCascade.detectMultiScale(gray, 1.3,5)
+            for(x,y,w,h) in faces:
+                regionOfInterestGray = gray[y:y+h, x:x+w]
+                regionOfInterestColour = img[y:y+h, x:x+w]
+                eyes = self.eyeCascade.detectMultiScale(regionOfInterestGray)
+                if len(eyes) >= 2:
+                    return regionOfInterestColour
