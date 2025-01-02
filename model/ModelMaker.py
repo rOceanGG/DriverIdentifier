609781c66944f630342c9e3ee26dc988687000b0
rOceanGG <roshangg@gmail.com>
2025-01-02 22:45:29 +0000

    Made changes to arrays and dictionaries in the getFaces function such that CroppedImageDirectories and DriverFileNamesDictionary are available to the whole class instead of just the function. Also created a function to quickly and easily create a numerical value for each driver that is stored in classificationDictionary

--- model/ModelMaker.py
+++ model/ModelMaker.py
@@ -12,6 +12,9 @@ class ModelMaker:
         self.eyeCascade  = cv2.CascadeClassifier('DriverIdentifier/model/opencv/haarcascade/haarcascade_eye.xml')
         self.DATAPATH = "./model/dataset/"
         self.CROPPEDDATAPATH = "./model/dataset/cropped/"
+        self.classificationDictionary = {}
+        self.CroppedImageDirectories = []
+        self.DriverFileNamesDictionary = {}
 
     def getFaces(self):
         #This function will be used on individual images to grab the faces of the drivers
@@ -38,8 +41,6 @@ class ModelMaker:
         
         # As the names suggest, these hold both the directories with cropped images
         # and a way to check which directory holds the images for each driver
-        CroppedImageDirectories = []
-        DriverFileNamesDictionary = {}
 
         for imageDirectory in ImageDirectories:
             # When creating the new file names, we need to ensure that each one is unique.
@@ -49,14 +50,14 @@ class ModelMaker:
             # When populating my dataset, the folder that contains the images of each driver, is named after the driver itself. 
             # Hence, the name of the driver can be found by grabbing the folder name
             driver = imageDirectory.split('/')[-1]
-            DriverFileNamesDictionary[driver] = []
+            self.DriverFileNamesDictionary[driver] = []
             croppedFolder = self.CROPPEDDATAPATH + driver
 
             # Creates the folder to hold the cropped images
             if not os.path.exists(croppedFolder):
                 os.makedirs(croppedFolder)
             
-            CroppedImageDirectories.append(croppedFolder)
+            self.CroppedImageDirectories.append(croppedFolder)
 
             for entry in os.scandir(imageDirectory):
                 try:
@@ -74,7 +75,7 @@ class ModelMaker:
                     # Saves the cropped image in the new folder
                     cv2.imwrite(newFilePath, croppedImage)
                     # Adds the new file name to the list of file names for the current driver
-                    DriverFileNamesDictionary[driver].append(newFileName)
+                    self.DriverFileNamesDictionary[driver].append(newFileName)
                     count += 1
 
     def waveletTransform(image, mode='haar', level = 1):
@@ -100,3 +101,9 @@ class ModelMaker:
         imageArrayHaar = np.uint8(imageArrayHaar)
         
         return imageArrayHaar
+    
+    def createClassificationDictionary(self):
+        count = 0
+        for driverName in self.DriverFileNamesDictionary.keys():
+            self.classificationDictionary[driverName] = count
+            count += 1
